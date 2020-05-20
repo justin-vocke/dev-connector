229bf09414ed9b2eac53d3ce6086fd5a72793693
Justin <therealmathhelper@gmail.com>
2020-05-20 17:04:34 -0400

    Put in user authorization using JWT

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,19 @@
 const express=require('express');
 const router=express.Router();
+const auth=require('../../middleware/auth')
 
 //@route  GET api/ath
 //@desc   Test route
 //@access public
-router.get('/',(req,res)=>res.send('Auth Route'));
+router.get('/',auth,
+async (req,res)=>{
+  try{
+    const user=await User.findById(req.user.id).select('-password');
+    res.json(user);
+  }catch(err){
+    console.log(err.message);
+    res.status(500).send('Server error');
+  }
+});
 
 module.exports=router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,9 @@ const router=express.Router();
 const {check,validationResult} = require('express-validator');
 const gravatar=require('gravatar');
 const bcrypt=require('bcryptjs');
+const jwt=require('jsonwebtoken');
+const config=require('config');
+
 
 const User=require('../../models/User');
 //@route  POST api/users
@@ -51,8 +54,22 @@ user.password=await bcrypt.hash(password,salt);
 
 await user.save();
 
-//return jsonwebtoken
-res.send('User registered');
+const payload={
+  user:{
+    id:user.id,
+  }
+}
+
+jwt.sign(
+  payload,
+  config.get('jwtSecret'),
+  {expiresIn:36000},
+  (err,token)=>{
+    if(err) throw err;
+    res.json({token});
+  });
+
+
 }catch(err){
   console.log(err.message);
   res.status(500).send('Server Error');
